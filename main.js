9ac84aa2ce3a7c11fde0a82881aeaf4b5c060cd8
n7x3n <m84380337@gmail.com>
2026-07-23 11:48:29 +0200

    rewrote css & added dialog

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import { wageCalculator } from './calc_engine.js';
+//mapování
+const addBtn = document.getElementById('add-btn');
+const cnlBtn = document.getElementById('cancel-btn');
+//
+
+addBtn.addEventListener('click', () =>{
+    document.getElementById('job-modal').showModal();
+})
+
+cnlBtn.addEventListener('click', () =>{
+    document.getElementById('job-modal').close();
+})
